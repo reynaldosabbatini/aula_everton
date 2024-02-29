b7ad99817a7f69ca0b28056c786d1805731c8b1c
reynaldosabbatini <161661677+reynaldosabbatini@users.noreply.github.com>
2024-02-28 22:42:49 -0300

    aula 28-02

---
+++ src/ex_2/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function mensagem () {
+    return(
+        <vieW>
+            <text>hello react-native</text>
+        </vieW>
+    );
+}
